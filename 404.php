84aa30949222685623def74e39ee142284f3e830
Josue Daniel Bustamante <josuedanielbustamante@gmail.com>
2014-06-15 23:08:15 -0500

    Add 404 Support

--- 404.php
+++ 404.php
@@ -14,7 +14,8 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 no-results not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h1>
+					<h3 class="entry-super-title">404</h3>
+					<h4 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h4>
 				</header>
 
 				<div class="entry-content">
